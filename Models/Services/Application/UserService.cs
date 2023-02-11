a91eb659cb64d0cbe0f4061933e6e2e961913eaf
NOODLS\s.riggi <s.riggi@publictechnlogies.com>
2023-02-11 14:22:51 +0100

    context

--- Models/Services/Application/UserService.cs
+++ Models/Services/Application/UserService.cs
@@ -2,34 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GymApp.Models.Services.Infrastructure;
 using GymApp.Models.ViewModels;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace GymApp.Models.Services.Application
 {
-    public class UserService
+    public class UserService: AbstractUserService
     {
         
-        public UserViewModel GetServices()
+        private readonly IMongoCollection<UserViewModel> _users;
+        public UserService(IOptions<AppDatabaseSettings> appDatabaseSettings):base(appDatabaseSettings)
         {
-            var list = new List<UserViewModel>();
-            var user = new UserViewModel()
-            {
-                Id = 10,
-                Name="Simone",
-                Surname = "Riggi",
-                Email = "simone.riggi92@gmail.com",
-                Username = "Simo92",
-                Password ="1234",
-                BithDay = DateTime.Parse("14 Oct 1992"),
-                PhoneNumber="+393202795763",
-                Country="Italy",
-                StateRegion="San Cataldo",
-                PlansCompleted= 12,
-                ImagePath = "/profile.jpeg"
-            };
+            MongoClient client = new MongoClient(appDatabaseSettings.Value.ConnectionString);
+            IMongoDatabase database = client.GetDatabase(appDatabaseSettings.Value.DatabaseName);
+            _users = database.GetCollection<UserViewModel>(appDatabaseSettings.Value.CollectionName);
+        }
+        public override UserViewModel GetUser(string Id)
+        {
+            // var list = new List<UserViewModel>();
+            // var user = new UserViewModel()
+            // {
+            //     Id = 10,
+            //     Name="Simone",
+            //     Surname = "Riggi",
+            //     Email = "simone.riggi92@gmail.com",
+            //     Username = "Simo92",
+            //     Password ="1234",
+            //     BirthDay = DateTime.Parse("14 Oct 1992"),
+            //     PhoneNumber="+393202795763",
+            //     Country="Italy",
+            //     StateRegion="San Cataldo",
+            //     PlansCompleted= 12,
+            //     ImagePath = "/profile.jpeg"
+            // };
+            var user = _usersCollection;
             // list.Add(user);
             // return list;
             return user;
+        }
+
+        public async override Task<List<UserViewModel>> GetUsers()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using GymApp.Models.Services.Application;
+using GymApp.Models.Services.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddMvc();
+builder.Services.AddTransient<AbstractUserService, UserService>(); 
+builder.Services.Configure<AppDatabaseSettings>(builder.Configuration.GetSection("AppDatabase"));
+builder.Services.AddSingleton<AbstractUserService>();
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -33,3 +41,5 @@ app.MapControllerRoute(
 app.MapControllers();
 
 app.Run();
+
+
